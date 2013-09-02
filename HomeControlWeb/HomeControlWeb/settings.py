4d119aae1ff06085a94129727a5b5f104d80cbc0
Itamar Ostricher <itamarost@gmail.com>
2013-09-02 16:55:29 +0300

    added console log handler for AC app logging

--- HomeControlWeb/HomeControlWeb/settings.py
+++ HomeControlWeb/HomeControlWeb/settings.py
@@ -145,13 +145,21 @@ LOGGING = {
             'level': 'ERROR',
             'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler'
+        },
     },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
+        },
+        'AC': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
         },
     }
 }
