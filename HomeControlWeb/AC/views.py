4be2c768a156618535bba30a7ce1d54eb7bfa8f0
Itamar O <itamarost@gmail.com>
2013-10-30 11:37:18 +0200

    removed redundant import

--- HomeControlWeb/AC/views.py
+++ HomeControlWeb/AC/views.py
@@ -3,7 +3,7 @@ import logging
 import json
 
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
 
 from AC.models import AcControl
