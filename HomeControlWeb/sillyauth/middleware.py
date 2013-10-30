616cb9e7764688e53d6a3aa2557921979b51c0b6
Itamar O <itamarost@gmail.com>
2013-10-30 10:07:34 +0200

    sillyauth should ignore paths of unsupported apps

--- HomeControlWeb/sillyauth/middleware.py
+++ HomeControlWeb/sillyauth/middleware.py
@@ -2,18 +2,21 @@ from django.conf import settings
 from django.core.exceptions import PermissionDenied
 from django.core.exceptions import MiddlewareNotUsed
 
-# The secret key for accessing the Silly Auth protected sites
-#  (default is disabled)
-SILLY_AUTH_PASS = getattr(settings, 'SILLY_AUTH_PASS', None)
-
 class SillyAuthMiddleware(object):
     def __init__(self):
-        if SILLY_AUTH_PASS is None:
+        # The secret key for accessing the Silly Auth protected sites
+        #  (default is disabled)
+        self._pass = getattr(settings, 'SILLY_AUTH_PASS', None)
+        if self._pass is None:
             raise MiddlewareNotUsed()
+        self._ignore_paths = set(['/admin/'])
 
     def process_request(self, request):
         "Verify password for Silly Auth authentication and save it in request"
+        for ignored_path in self._ignore_paths:
+            if request.path.startswith(ignored_path):
+                return
         key = request.GET.get('key', '')
-        if key != SILLY_AUTH_PASS:
+        if key != self._pass:
             raise PermissionDenied()
         request.silly_auth_pass = key
